440e68ffdbf8c2f4a1fdfc4c4eee529bc5cab88a
Michael Joy <kodiakchaos@gmail.com>
2023-04-24 21:30:39 -0400

    watcher comment

--- src/composables/useChartTimeWatcher.ts
+++ src/composables/useChartTimeWatcher.ts
@@ -1,8 +1,12 @@
 import type { ChartTime } from '@/types/misc'
 import { watch } from 'vue'
 import useUIStore from '@/stores/ui'
-import useLogger from './useLogger'
+import useLogger from '@/composables/useLogger'
 
+/**
+ * Composable with the watcher for triggering chart updates.
+ * @param chartUpdateFunc
+ */
 export default function useChartTimeWatcher(chartUpdateFunc: () => Promise<void>) {
   const uiStore = useUIStore()
   const { log } = useLogger()
